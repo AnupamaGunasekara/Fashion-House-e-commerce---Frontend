dbf7d67122191a4e3b853e23b353ae2fe9111519
Anupama Gunasekara <87498573+AnupamaGunasekara@users.noreply.github.com>
2024-11-13 23:44:33 +0530

    private routs checkup

--- src/Routers/router.jsx
+++ src/Routers/router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, Route } from 'react-router-dom';
 import App from '../App';
 import Login from '../Components/Login';
 import Register from '../Components/Register';
+import DashboardLayout from '../pages/Dashboard/DashboardLayout';
 
 const router = createBrowserRouter([
   {
@@ -22,7 +23,28 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register/> 
-  }
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />, // TODO: user private routes here
+    children: [
+        // user routes
+        { path: "", element: <div>User Dashboard</div> },
+        { path: "orders", element: <div>User Orders</div> },
+        { path: "payments", element: <div>User Payments</div> },
+        { path: "profile", element: <div>User Profile</div> },
+        { path: "reviews", element: <div>User Reviews</div> },
+
+        // admin routes (only accessible by admin) TODO: private routes with role field
+        { path: "admin", element: <div>Admin Main</div> },
+        { path: "add-new-post", element: <div>New Post</div> },
+        { path: "manage-products", element: <div>Manage Products</div> },
+        { path: "update-product/:id", element: <div>Update Product</div> },
+        { path: "users", element: <div>All Users</div> },
+        { path: "manage-orders", element: <div>Manage Orders</div> }
+    ]
+}
+
 ]);
 
 export default router;
