abd82eb9828ca71e3a127d53b12e0d58ddc48a85
kesara <udeshan521@gmail.com>
2024-11-13 15:23:14 +0530

    Develop Search Page Functions

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
 import CategoryPage from "../pages/category/CategoryPage";
+import Search from "../pages/search/Search";
+import ShopPage from "../pages/shop/ShopPage";
 
 const router = createBrowserRouter([
     {
@@ -8,7 +10,9 @@ const router = createBrowserRouter([
       element: <App/>,
       children: [
         {path: "/", element:<div>Home Page</div>},
-        { path: "/categories/:categoryName", element: <CategoryPage /> },
+        {path: "/categories/:categoryName", element: <CategoryPage /> },
+        { path: "/search", element: <Search /> },
+        { path: "/shop", element: <ShopPage /> },
       ]
     },
   ]);
